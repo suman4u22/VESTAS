dcd185aee51a6a7eb91718840352788e376700c4
suman4u22 <47024589+suman4u22@users.noreply.github.com>
2019-04-02 17:11:05 +0530

    Update and rename Remote to Remote.ps1

--- Remote.ps1
+++ Remote.ps1
@@ -1,5 +1,5 @@
-echo "Connecting to 10.211.104.20"
-$Server="10.211.104.20"
+echo "Connecting to 10.211.104.47"
+$Server="10.211.104.47"
 $User="Administrator"
 $Password="only4YOU"
 cmdkey /generic:TERMSRV/$Server /user:$User /pass:$Password
